76f94327d87927d8622d5f4bb79ef201a8ae4f61
Christian Gulden <christian.gulden@fau.de>
2020-01-06 16:03:55 +0100

    test(e2e): wait for FHIR 'List' request to have occurred before testing

--- tests/e2e/specs/PatientRecommendations.spec.js
+++ tests/e2e/specs/PatientRecommendations.spec.js
@@ -1,14 +1,34 @@
 // https://docs.cypress.io/api/introduction/api.html
 
+const listRequestUrl =
+  "**/List?code=http://miracum.org/fhir/CodeSystem/screening-list|screening-recommendations";
+
 describe("Homepage is loading correctly", () => {
   it("Two tabs for the sample studies exist", () => {
-    cy.visit("/");
+    cy.server();
+    cy.route("GET", listRequestUrl).as("getLists");
+    cy.visit("/", {
+      onBeforeLoad: (win) => {
+        // eslint-disable-next-line no-param-reassign
+        win.fetch = null;
+      },
+    });
+    cy.wait("@getLists");
 
-    cy.get(".b-tabs > nav > ul > li", { timeout: 60000 }).should("have.length", 2);
+    cy.get(".b-tabs > nav > ul > li", { timeout: 30000 }).should("have.length", 2);
   });
 
   it("Can switch between the two study tabs", () => {
-    cy.visit("/");
+    cy.server();
+    cy.route("GET", listRequestUrl).as("getLists");
+    cy.visit("/", {
+      onBeforeLoad: (win) => {
+        // eslint-disable-next-line no-param-reassign
+        win.fetch = null;
+      },
+    });
+
+    cy.wait("@getLists");
 
     cy.contains("Krankheitslast und Versorgungssituation");
 
