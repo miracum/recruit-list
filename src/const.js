7192732483dabd1161271e05e87c957fbb8a7725
Christian Gulden <christian.gulden@fau.de>
2020-04-19 16:42:09 +0000

    fix: updated screening list code system

--- src/const.js
+++ src/const.js
@@ -1,7 +1,7 @@
 export default {
   SYSTEM_SUBJECT_IDENTIFIER: "http://ohdsi.org/omop/fhir/subject-identifier",
   SYSTEM_STUDY_ACRONYM: "https://fhir.miracum.org/uc1/StructureDefinition/StudyAcronym",
-  SYSTEM_SCREENING_LIST: "https://fhir.miracum.org/uc1/CodeSystem/ScreeningList",
+  SYSTEM_SCREENING_LIST: "https://fhir.miracum.org/uc1/CodeSystem/screeningList",
   URL_NOTE_EXTENSION: "https://fhir.miracum.org/uc1/StructureDefinition/ResearchSubjectNote",
   STATUS_TRANSLATION: {
     candidate: "Rekrutierungsvorschlag",
